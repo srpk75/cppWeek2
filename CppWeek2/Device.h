7f2710b981da5f4c37bf660a11fc6a296ac2da5f
srpk75 <avodaalgamal@gmail.com>
2024-11-25 18:58:17 +0200

    added Device.h and some fields

---
+++ CppWeek2/Device.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <string>
+
+///////////////////////////////////////////////////////////
+/*														 //
+	Those definitions are used for tests,                //
+	you can add your own, but please do not delete any.  //
+*/														 //
+enum DeviceType { PHONE, PC, LAPTOP, TABLET };		     //
+//
+#define UbuntuOS "UbuntuLinux"                           //						
+#define RedHatOS "RedHatLinux"							 //
+#define MacOS "MacOS"									 //
+#define IOS "IOS"										 //
+#define WINDOWS7 "Windows7"                              //
+#define WINDOWS10 "Windows10"                            //
+#define WINDOWS11 "Windows11"                            //
+#define ANDROID "Android"                                //
+														 //
+/// ///////////////////////////////////////////////////////
+
+
+class Device
+{
+public:
+
+
+private:
+	bool isActive;
+	std::string _ip;
+	std::string _type
+
+};

515fa8d2846b2edd63954524caa96ff3ecba767e
srpk75 <avodaalgamal@gmail.com>
2024-11-25 22:47:51 +0200

    started user didnt do much going to sleep

--- CppWeek2/User.h
+++ CppWeek2/User.h
@@ -1,1 +1,19 @@
 #pragma once
+#include <string>
+#include "Device.h"
+
+class User
+{
+public:
+	void init(const unsigned int id, const std::string username, const unsigned int age);
+	void clear();
+	unsigned int getID() const;
+	std::string getUserName() const;
+	unsigned int getAge() const;
+
+private:
+	int _id;
+	std::string _username;
+	int _age;
+	Device _device;
+};

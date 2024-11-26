2d1e32740b435e85cdc2a9042d9d955fb287e9df
srpk75 <avodaalgamal@gmail.com>
2024-11-26 09:48:16 +0200

    implemented .h functions for user

---
+++ CppWeek2/User.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include "User.h"
+
+void User::init(const unsigned int id, const std::string username, const unsigned int age)
+{
+	this->_id = id;
+	this->_username = username;
+	this->_age = age;
+}
+
+void User::clear()
+{
+	//currently nothing to clear
+}
+
+unsigned int User::getID() const
+{
+	return this->_id;
+}
+
+std::string User::getUserName() const
+{
+	return this->_username;
+}
+
+unsigned int User::getAge() const
+{
+	return this->_age;
+}
